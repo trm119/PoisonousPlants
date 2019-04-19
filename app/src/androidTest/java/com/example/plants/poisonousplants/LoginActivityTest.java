f3db8a4778bd33ff1b2bc5fad8cad23506c9b3f4
rtp25 <42896428+rtp25@users.noreply.github.com>
2019-04-19 12:43:39 -0500

    Added some unit test to Regiser and Login Activities

--- app/src/androidTest/java/com/example/plants/poisonousplants/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/plants/poisonousplants/LoginActivityTest.java
@@ -3,28 +3,23 @@ package com.example.plants.poisonousplants;
 import android.content.Context;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
 import com.example.plants.poisonousplants.View.activities.LoginActivity;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-//import androidx.test.core.app.ApplicationProvider;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
 
-    //private Context context = LoginActivity.getApplicationContext();
 
     @Rule
     public ActivityTestRule<LoginActivity> loginActivityRule =
             new ActivityTestRule<>(LoginActivity.class);
 
     @Test
-    public void testValidateUserEmailInput() {
+    public void testValidateEmailInput_Format_True() {
 
         //LoginActivity log_act = new LoginActivity(mMockContext);
         String example_email = "user1@email.com";
@@ -33,12 +28,29 @@ public class LoginActivityTest {
     }
 
     @Test
-    public void testValidateUserPasswordInput() {
+    public void testValidatePasswordInput_Length_True() {
 
         String example_password = "password";
         boolean result = loginActivityRule.getActivity().validateUserPasswordInput(example_password);
         assertTrue(result);
     }
 
+    @Test
+    public void testValidateEmailInput_Format_False() {
+
+        //LoginActivity log_act = new LoginActivity(mMockContext);
+        String example_email = "u.com";
+        boolean result  = loginActivityRule.getActivity().validateUserEmailInput(example_email);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testValidatePasswordInput_Length_False() {
+
+        String example_password = "paw";
+        boolean result = loginActivityRule.getActivity().validateUserPasswordInput(example_password);
+        assertTrue(result);
+    }
 
 }
+
--- app/src/androidTest/java/com/example/plants/poisonousplants/RegisterActivityTest.java
+++ app/src/androidTest/java/com/example/plants/poisonousplants/RegisterActivityTest.java
@@ -3,38 +3,32 @@ package com.example.plants.poisonousplants;
 import android.content.Context;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
 import com.example.plants.poisonousplants.View.activities.LoginActivity;
 import com.example.plants.poisonousplants.View.activities.RegisterActivity;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-//import androidx.test.core.app.ApplicationProvider;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class RegisterActivityTest {
 
-    //private Context context = LoginActivity.getApplicationContext();
 
     @Rule
     public ActivityTestRule<RegisterActivity> registerActivityRule =
             new ActivityTestRule<>(RegisterActivity.class);
 
     @Test
-    public void testValidateUserEmailInput() {
+    public void testValidateEmailInput_Format_True() {
 
-        //LoginActivity log_act = new LoginActivity(mMockContext);
         String example_email = "user1@email.com";
         boolean result  = registerActivityRule.getActivity().validateEmailInput(example_email);
         assertTrue(result);
     }
 
     @Test
-    public void testValidateUserPasswordInput() {
+    public void testValidatePasswordInput_Length_True() {
 
         String example_password = "userpassword";
         boolean result = registerActivityRule.getActivity().validatePasswordInput(example_password);
@@ -42,12 +36,36 @@ public class RegisterActivityTest {
     }
 
     @Test
-    public void testValidateUserNameInput() {
+    public void testValidateUserNameInput_Length_True() {
 
         String example_password = "username";
         boolean result = registerActivityRule.getActivity().validatePasswordInput(example_password);
         assertTrue(result);
     }
 
+    @Test
+    public void testValidateEmailInput_Format_False() {
+
+        //LoginActivity log_act = new LoginActivity(mMockContext);
+        String example_email = "u.com";
+        boolean result  = registerActivityRule.getActivity().validateEmailInput(example_email);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testValidatePasswordInput_Length_False() {
+
+        String example_password = "pas";
+        boolean result = registerActivityRule.getActivity().validatePasswordInput(example_password);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testValidateUserNameInput_Length_False() {
+
+        String example_password = "us";
+        boolean result = registerActivityRule.getActivity().validatePasswordInput(example_password);
+        assertTrue(result);
+    }
 
 }
