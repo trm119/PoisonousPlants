0f207504267a99344793f9c0291501cc28b0c2f0
trm119 <taylorrm255@gmail.com>
2019-05-02 18:09:34 -0500

    features

--- app/src/main/java/com/example/plants/poisonousplants/View/activities/MainActivity.java
+++ app/src/main/java/com/example/plants/poisonousplants/View/activities/MainActivity.java
@@ -8,11 +8,25 @@ import android.widget.FrameLayout;
 import com.example.plants.poisonousplants.R;
 import com.example.plants.poisonousplants.View.helpers.ShowCamera;
 
+/**
+ * The class is the main entrance to the application that hosts the camera.
+ *
+ * @author  Taylor Mauldin
+ * @version 1.0
+ * @since   2019-04-06
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     Camera camera;
     FrameLayout frameLayout;
     ShowCamera showCamera;
+
+    /** On startup, display the camera to the user.
+     *@param savedInstanceState A Bundle object that can restore the
+     * activity to a previous state.
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/plants/poisonousplants/View/helpers/ShowCamera.java
+++ app/src/main/java/com/example/plants/poisonousplants/View/helpers/ShowCamera.java
@@ -8,10 +8,25 @@ import android.view.SurfaceView;
 
 import java.io.IOException;
 
+
+/**
+ * The class handles functions for the camera operations.
+ *
+ * @author  Taylor Mauldin
+ * @version 1.0
+ * @since   2019-04-05
+ */
+
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
     Camera camera;
     SurfaceHolder holder;
 
+
+    /**
+     * Creates the container for the camera and displays
+     * @param camera Native android camera object
+     * @param context Environment handler
+     */
     public ShowCamera(Context context, Camera camera) {
         super(context);
         this.camera = camera;
@@ -20,6 +35,11 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
     }
 
+    /**
+     * Sets camera settings when it is created and displayed
+     * @param holder Container for camera
+     *
+     */
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Camera.Parameters params = camera.getParameters();
@@ -29,8 +49,8 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
         if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
         {
             params.set("orientation", "portrait");
-            camera.setDisplayOrientation(90);
-            params.setRotation(90);
+            camera.setDisplayOrientation(270);
+            //params.setRotation(90);
         }
         else
         {
